0753210707372143b4d7ca1d56a4920c3844ac19
damon <41898000+notcat@users.noreply.github.com>
2022-02-26 23:57:38 -0800

    finished reading file and looping example in main()

--- src/main.rs
+++ src/main.rs
@@ -1,34 +1,91 @@
+use std::io::ErrorKind;
+use std::time::Duration;
 use std::{
-    fmt::Error,
+    error::Error,
     fs::{self, File},
     io::{self, Read},
 };
+
 use thiserror::Error as ThisError;
+
+// steal specners structopt stuff and use that to fill out the PATH_TO_BACKLIGHT instead
+// so you could just call ./rpi-backlight-dimmer --path /sys/class/backlight/rpi-backlight/brightness
 
 const PATH_TO_BACKLIGHT: &str = "C:/Users/damon/OneDrive/Rust/rpi-backlight-dimmer/brightness";
 
 #[derive(Debug, ThisError)]
 pub enum RpiError {
-    #[error("Unable to read file")]
+    #[error("generic file error")]
+    FileError,
+    //FileError(#[from] io::Error),
+    #[error("missing permissions to the file!")]
+    MissingPermissions,
+    #[error("file not found!")]
+    FileMissing,
+    // See line 39
+    // #[error("file was busy")]
+    // FileBusy,
+    #[error("error while reading file contents")]
     ReadError,
+    #[error("unable to parse file into a number value")]
+    ParseError,
 }
 
-fn main() {
-    let brightness = match read_backlight_from_file() {
-        Ok(value) => value, // returning the value given from the result, and this sets the brightness variable
-        Err(_) => RpiError::ReadError.to_string(),
-    };
+fn main() -> Result<(), Box<dyn Error>> {
+    loop {
+        let brightness = match read_backlight_from_file() {
+            Ok(value) => value,
+            Err(error) => match error {
+                // Soon will be available in a new version of rust (currently on nightly only https://github.com/rust-lang/rust/issues/86442)
+                // RpiError::FileBusy => {
+                //     dbg!("got minor error!: {}", error);
+                //     dbg!("retrying in 3 seconds, (file could just be busy)");
 
-    dbg!(brightness);
-    //println!("{}", data)
+                //     std::thread::sleep(Duration::from_secs(3));
 
-    //Err(RpiError::ReadError);
+                //     continue;
+                // }
+                RpiError::FileError | RpiError::FileMissing | RpiError::ParseError => {
+                    panic!("{}", error)
+                }
+                other => {
+                    dbg!("got minor error!: {}", other);
+                    dbg!("retrying in 3 seconds, (file could just be busy)");
+
+                    std::thread::sleep(Duration::from_secs(3));
+
+                    continue;
+                }
+            },
+        };
+
+        dbg!(brightness);
+        std::thread::sleep(Duration::from_secs(1));
+    }
 }
 
-fn read_backlight_from_file() -> Result<String, io::Error> {
+fn read_backlight_from_file() -> Result<u8, RpiError> {
     let mut backlight_value = String::new();
 
-    File::open(PATH_TO_BACKLIGHT)?.read_to_string(&mut backlight_value)?;
+    let file = File::open(PATH_TO_BACKLIGHT); // .read_to_string(&mut backlight_value)
 
-    Ok(backlight_value)
+    let file: Result<_, RpiError> = match file {
+        Ok(mut handle) => match handle.read_to_string(&mut backlight_value) {
+            Ok(_) => Ok(()),
+            Err(_) => return Err(RpiError::ReadError),
+        },
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => return Err(RpiError::FileMissing),
+            ErrorKind::PermissionDenied => return Err(RpiError::MissingPermissions),
+            // See line 39
+            //ErrorKind::ResourceBusy => return Err(RpiError::FileBusy),
+            _ => return Err(RpiError::FileError),
+        },
+    };
+
+    let result = backlight_value
+        .parse::<u8>()
+        .map_err(|_| RpiError::ParseError)?;
+
+    Ok(result)
 }
