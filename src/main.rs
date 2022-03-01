c243fc804abf2a9db3f7d7247305ba69599ea183
damon <41898000+notcat@users.noreply.github.com>
2022-02-28 19:40:06 -0800

    Revert "added the brightness value changing throughout the day"
    This reverts commit 6e4355776d7f4266db421422c569e99e69fb3617.

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,17 @@
+use std::error;
 use std::f32::consts::PI;
 use std::io::{self, ErrorKind, Write};
-use std::num::ParseIntError;
 use std::str::Bytes;
 use std::time::Duration;
-use std::{any, error, time};
 use std::{error::Error, fs::File, io::Read};
 
 use chrono::{self, Local, Timelike};
 use thiserror::Error as ThisError;
 
-// TODO: steal specners structopt stuff and use that to fill out the PATH_TO_BACKLIGHT instead
+// steal specners structopt stuff and use that to fill out the PATH_TO_BACKLIGHT instead
 // so you could just call ./rpi-backlight-dimmer --path /sys/class/backlight/rpi-backlight/brightness
 
-const PATH_TO_BACKLIGHT: &str = "C:/Users/damooo/OneDrive/Rust/rpi-backlight-dimmer/brightness";
+const PATH_TO_BACKLIGHT: &str = "C:/Users/damon/OneDrive/Rust/rpi-backlight-dimmer/brightness";
 
 #[derive(Debug, ThisError)]
 pub enum RpiError {
@@ -29,41 +28,13 @@ pub enum RpiError {
     #[error("error while reading file contents")]
     ReadError,
     #[error("unable to parse file into a number value")]
-    ParseError(ParseIntError),
+    ParseError,
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    let mut backlight = RpiBacklight::new(PATH_TO_BACKLIGHT);
-
-    match backlight.read_backlight_from_file() {
-        Ok(value) => println!("value: {}", value),
-        Err(error) => match error {
-            // Soon will be available in a new version of rust (currently on nightly only https://github.com/rust-lang/rust/issues/86442)
-            // RpiError::FileBusy => {
-            //     dbg!("got minor error!: {}", error);
-            //     dbg!("retrying in 3 seconds, (file could just be busy)");
-
-            //     std::thread::sleep(Duration::from_secs(3));
-
-            //     continue;
-            // }
-            RpiError::FileError | RpiError::FileMissing => {
-                panic!("{}", error);
-            }
-            RpiError::ParseError(error) => {
-                dbg!(error.clone());
-                panic!("{}", error);
-            }
-            other => {
-                dbg!("got minor error!", other);
-                dbg!("retrying in 3 seconds, (file could just be busy)");
-
-                std::thread::sleep(Duration::from_secs(3));
-            }
-        },
-    };
-
     loop {
+        let mut backlight = RpiBacklight::new(PATH_TO_BACKLIGHT);
+
         let brightness = match backlight.read_backlight_from_file() {
             Ok(value) => value,
             Err(error) => match error {
@@ -76,12 +47,8 @@ fn main() -> Result<(), Box<dyn Error>> {
 
                 //     continue;
                 // }
-                RpiError::FileError | RpiError::FileMissing => {
-                    panic!("{}", error);
-                }
-                RpiError::ParseError(error) => {
-                    dbg!(error.clone());
-                    panic!("{}", error);
+                RpiError::FileError | RpiError::FileMissing | RpiError::ParseError => {
+                    panic!("{}", error)
                 }
                 other => {
                     dbg!("got minor error!", other);
@@ -94,19 +61,9 @@ fn main() -> Result<(), Box<dyn Error>> {
             },
         };
 
-        let mut new_value = backlight.change_backlight_off_dayminute();
-
-        if new_value == brightness {
-            std::thread::sleep(Duration::from_secs(1));
-            continue;
-        }
-
-        if let Err(error) = backlight.change_backlight_in_file(new_value) {
+        if let Err(error) = backlight.change_backlight_in_file(brightness + 5) {
             match error {
-                RpiError::FileError | RpiError::FileMissing => {
-                    panic!("{}", error)
-                }
-                RpiError::ParseError(error) => {
+                RpiError::FileError | RpiError::FileMissing | RpiError::ParseError => {
                     panic!("{}", error)
                 }
                 other => {
@@ -120,7 +77,8 @@ fn main() -> Result<(), Box<dyn Error>> {
             }
         }
 
-        println!("old value: {} -> new value: {}", brightness, new_value);
+        backlight.change_backlight_off_dayminute();
+        dbg!(brightness);
 
         std::thread::sleep(Duration::from_secs(1));
     }
@@ -156,7 +114,7 @@ impl RpiBacklight {
 
         let result = backlight_string
             .parse::<u8>()
-            .map_err(|error| RpiError::ParseError(error))?;
+            .map_err(|_| RpiError::ParseError)?;
 
         Ok(result)
     }
@@ -167,7 +125,7 @@ impl RpiBacklight {
         //     Err(err) => return Err(err),
         // };
 
-        let _file = File::options().write(true).truncate(true).open(self.path); // .read_to_string(&mut backlight_value)
+        let _file = File::options().write(true).open(self.path); // .read_to_string(&mut backlight_value)
 
         match _file {
             Ok(mut handle) => match handle.write_all(value.to_string().as_bytes()) {
@@ -180,7 +138,7 @@ impl RpiBacklight {
         // lerp (current value, future value, time)
     }
 
-    fn change_backlight_off_dayminute(&mut self) -> u8 {
+    fn change_backlight_off_dayminute(&mut self) {
         // chrono
         // time of day in MINUTES
         // convert minutes to 0-1 value
@@ -189,33 +147,13 @@ impl RpiBacklight {
 
         let minutes_zero_to_one =
             ((Local::now().hour() * 60) + Local::now().minute()) as f32 / 1440.0;
-        // let minutes_zero_to_one = Local::now().second() as f32 / 60.0;
 
-        // TODO: make this a config file option
-        let timestates = TimeStates {
-            sunrise: 0.0,
-            daytime: 0.3333333,
-            dusk: 0.6666666,
-        };
+        let calculated_brightness = -0.5 * f32::cos(PI * minutes_zero_to_one) + 0.5;
 
-        let mut calculated_brightness: f32 = 0.0;
+        //let timestates = TimeStates;
 
-        // Beginning rising curve
-        if minutes_zero_to_one > timestates.sunrise && minutes_zero_to_one < timestates.daytime {
-            calculated_brightness = -0.5 * f32::cos(PI * minutes_zero_to_one * 3.0) + 0.5;
-        }
-
-        // Constant High
-        if minutes_zero_to_one > timestates.daytime && minutes_zero_to_one < timestates.dusk {
-            calculated_brightness = 1.0;
-        }
-
-        // falling rising curve
-        if minutes_zero_to_one > timestates.dusk {
-            calculated_brightness = 0.5 * f32::cos(PI * minutes_zero_to_one * 3.0) + 0.5;
-        }
-
-        return (calculated_brightness * 250.0 + 5.0) as u8;
+        dbg!(calculated_brightness);
+        dbg!(minutes_zero_to_one);
     }
 
     fn map_error(error: io::Error) -> RpiError {
